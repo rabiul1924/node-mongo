953fbb4f60caa951ea6aee5ad0621e56f0279232
Md.Rabiul islam <rabiulrabiul1924@gmail.com>
2021-03-31 19:04:59 +0600

    port+nodemon

--- index.js
+++ index.js
@@ -61,5 +61,7 @@ app.get('/', (req, res) => {
  })
   
   
-  app.listen(3000, () => {console.log('listening to port 3000')} )
+//   app.listen(3000, () => {console.log('listening to port 3000')} )
+
+app.listen(process.env.PORT || port);
 
